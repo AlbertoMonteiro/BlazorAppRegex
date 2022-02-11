895a7a06bacfbb0d95d6c39e06919ffc12242d60
Alberto Monteiro <alberto.monteiro@live.com>
2022-02-11 14:21:40 -0300

    Using AOT and performance improvements

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,21 +1,34 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.JSInterop;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 _ = WebAssemblyHostBuilder.CreateDefault(args);
 
 public static class Sample
 {
-    [JSInvokable]
-    public static object SayHelloCS(string regex, string value)
+    [Microsoft.JSInterop.JSInvokable]
+    public static AllResults SayHelloCS(string regex, string value)
     {
-        var result = Regex.Match(value, regex);
-        return new
+        var date = System.DateTime.Now;
+        var results = Regex.Matches(value, regex);
+        var allResults = new Result[results.Count];
+        for (int i = 0; i < allResults.Length; i++)
         {
-            result.Success,
-            Captures = result.Captures.Cast<Capture>().Select(x => new { x.Index, x.Length, x.Value }),
-            Groups = result.Groups.Cast<Group>().Select(x => new { x.Index, x.Length, x.Success, x.Name, x.Value })
-        };
+            var result = results[i];
+            var regexGroups = result.Groups;
+            var groups = new RegexGroup[regexGroups.Count];
+            for (int j = 0; j < groups.Length; j++)
+            {
+                var currentGroup = regexGroups[j];
+                groups[j] = new RegexGroup(currentGroup.Index, currentGroup.Length, currentGroup.Success, currentGroup.Name, currentGroup.Value);
+            }
+            allResults[i] = new Result(result.Success, groups);
+        }
+
+        var ms = System.DateTime.Now.Subtract(date).TotalMilliseconds;
+        return new AllResults(allResults, ms);
     }
 }
+
+public record RegexGroup(int Index, int Length, bool Success, string Name, string Value);
+public record Result(bool Success, RegexGroup[] Groups);
+public record AllResults(Result[] allResults, double Elapsed);
