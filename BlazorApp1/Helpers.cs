a3c506d6df30fb262e4864b2795e6cd3d36961e4
Alberto Monteiro <alberto.monteiro@live.com>
2022-02-17 12:03:03 -0300

    Regex group names as string Escaped name and value if they have double quotes Few other enhancements

--- BlazorApp1/Helpers.cs
+++ BlazorApp1/Helpers.cs
@@ -1,4 +1,5 @@
 using Microsoft.JSInterop;
+using System.Runtime.CompilerServices;
 
 namespace BlazorApp1;
 
@@ -14,42 +15,46 @@ public static class Helpers
 
     [JSInvokable]
     public static void RegexMatches(string regex, string value)
-        => System.Threading.Tasks.Task.Run(() =>
-        {
-            var regexSp = ValueStopwatch.StartNew();
-            var results = System.Text.RegularExpressions.Regex.Matches(value, regex);
-            var builder = new System.Text.StringBuilder();
-            builder.Append("{\"matches\":[");
-            var i = 0;
-            var result = results[i];
-            builder.Append($"{{\"success\":{(result.Success ? TRUE : FALSE)},\"groups\":[");
-            var regexGroups = result.Groups;
-            var j = 0;
-            var currentGroup = regexGroups[j];
-            builder.Append($"{{\"index\":{currentGroup.Index},\"length\":{currentGroup.Length},\"Success\":{(currentGroup.Success ? TRUE : FALSE)},\"name\":{currentGroup.Name},\"value\":\"{currentGroup.Value}\"}}");
-            for (j++; j < regexGroups.Count; j++)
-            {
-                currentGroup = regexGroups[j];
-                builder.Append($",{{\"index\":{currentGroup.Index},\"length\":{currentGroup.Length},\"Success\":{(currentGroup.Success ? TRUE : FALSE)},\"name\":{currentGroup.Name},\"value\":\"{currentGroup.Value}\"}}");
-            }
-            builder.Append("]}");
+     => System.Threading.Tasks.Task.Run(() =>
+     {
+         var regexSp = ValueStopwatch.StartNew();
+         var results = System.Text.RegularExpressions.Regex.Matches(value, regex);
+         var builder = new System.Text.StringBuilder();
+         builder.Append("[");
+         var i = 0;
+         var result = results[i];
+         builder.Append($"{{\"success\":{(result.Success ? TRUE : FALSE)},\"groups\":[");
+         var regexGroups = result.Groups;
+         var j = 0;
+         var currentGroup = regexGroups[j];
+         builder.Append(CreateGroup(currentGroup));
+         for (j++; j < regexGroups.Count; j++)
+         {
+             currentGroup = regexGroups[j];
+             builder.Append($",{CreateGroup(currentGroup)}");
+         }
+         builder.Append("]}");
 
-            for (i++; i < results.Count; i++)
-            {
-                result = results[i];
-                builder.Append($",{{\"success\":{(result.Success ? TRUE : FALSE)},\"groups\":[");
-                regexGroups = result.Groups;
-                j = 0;
-                currentGroup = regexGroups[j];
-                builder.Append($"{{\"index\":{currentGroup.Index},\"length\":{currentGroup.Length},\"Success\":{(currentGroup.Success ? TRUE : FALSE)},\"name\":{currentGroup.Name},\"value\":\"{currentGroup.Value}\"}}");
-                for (j++; j < regexGroups.Count; j++)
-                {
-                    currentGroup = regexGroups[j];
-                    builder.Append($",{{\"index\":{currentGroup.Index},\"length\":{currentGroup.Length},\"Success\":{(currentGroup.Success ? TRUE : FALSE)},\"name\":{currentGroup.Name},\"value\":\"{currentGroup.Value}\"}}");
-                }
-                builder.Append("]}");
-            }
-            builder = builder.Append($"], \"elapsed\": {regexSp.GetElapsedTime().TotalMilliseconds} }}");
-            JsRuntime.InvokeUnmarshalled<string, int>("regexCallback", builder.ToString());
-        });
+         for (i++; i < results.Count; i++)
+         {
+             result = results[i];
+             builder.Append($",{{\"success\":{(result.Success ? TRUE : FALSE)},\"groups\":[");
+             regexGroups = result.Groups;
+             j = 0;
+             currentGroup = regexGroups[j];
+             builder.Append(CreateGroup(currentGroup));
+             for (j++; j < regexGroups.Count; j++)
+             {
+                 currentGroup = regexGroups[j];
+                 builder.Append($",{CreateGroup(currentGroup)}");
+             }
+             builder.Append("]}");
+         }
+         builder = builder.Append("]");
+
+         JsRuntime.InvokeUnmarshalled<string, int, int>("regexCallback", builder.ToString(), (int)regexSp.GetElapsedTime().TotalMilliseconds);
+
+         static string CreateGroup(System.Text.RegularExpressions.Group currentGroup)
+            => $"{{\"index\":{currentGroup.Index},\"length\":{currentGroup.Length},\"success\":{(currentGroup.Success ? TRUE : FALSE)},\"name\":\"{currentGroup.Name.Replace("\"", "\\\"")}\",\"value\":\"{currentGroup.Value.Replace("\"", "\\\"")}\"}}";
+     });
 }
