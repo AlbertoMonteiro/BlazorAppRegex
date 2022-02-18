ad6ab61a5b403e5b5057daa058d7be3a3531f142
Alberto Monteiro <alberto.monteiro@live.com>
2022-02-18 15:37:54 -0300

    Small change

--- BlazorApp1/Helpers.cs
+++ BlazorApp1/Helpers.cs
@@ -16,47 +16,21 @@ public static class Helpers
     private const string TRUE = "true";
     private const string FALSE = "false";
     public static IJSUnmarshalledRuntime JsRuntime = new CustomRuntime();
-    private static string _pattern;
-    private static string _value;
-    private static int _flags;
 
     public static void Init() => JsRuntime.InvokeUnmarshalled<string>("engineInit");
 
     [JSInvokable]
     [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
     public static void RegexMatches(string pattern, string value, int flags)
-    {
-        _pattern = pattern;
-        _value = value;
-        _flags = flags;
-        System.Threading.Tasks.Task.Run(RunRegexMatches);
-    }
-
-    [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
-    private static void RunRegexMatches()
-    {
-        var results = Regex.Matches(_value, _pattern, (RegexOptions)_flags);
-        var builder = new StringBuilder();
-        var i = 0;
-        var result = results[i];
-        builder.Append('[');
-        var regexGroups = result.Groups;
-        var j = 0;
-        builder.Append('[');
-        CreateGroup(regexGroups[j], builder, j);
-        for (j++; j < regexGroups.Count; j++)
+        => System.Threading.Tasks.Task.Run(() =>
         {
-            builder.Append(',');
-            CreateGroup(regexGroups[j], builder, j);
-        }
-        builder.Append(']');
-
-        for (i++; i < results.Count; i++)
-        {
-            result = results[i];
-            builder.Append(',');
-            regexGroups = result.Groups;
-            j = 0;
+            var results = Regex.Matches(value, pattern, (RegexOptions)flags);
+            var builder = new StringBuilder();
+            var i = 0;
+            var result = results[i];
+            builder.Append('[');
+            var regexGroups = result.Groups;
+            var j = 0;
             builder.Append('[');
             CreateGroup(regexGroups[j], builder, j);
             for (j++; j < regexGroups.Count; j++)
@@ -65,11 +39,26 @@ public static class Helpers
                 CreateGroup(regexGroups[j], builder, j);
             }
             builder.Append(']');
-        }
-        builder = builder.Append(']');
 
-        JsRuntime.InvokeUnmarshalled<string, int>("regexCallback", builder.ToString());
-    }
+            for (i++; i < results.Count; i++)
+            {
+                result = results[i];
+                builder.Append(',');
+                regexGroups = result.Groups;
+                j = 0;
+                builder.Append('[');
+                CreateGroup(regexGroups[j], builder, j);
+                for (j++; j < regexGroups.Count; j++)
+                {
+                    builder.Append(',');
+                    CreateGroup(regexGroups[j], builder, j);
+                }
+                builder.Append(']');
+            }
+            builder = builder.Append(']');
+
+            JsRuntime.InvokeUnmarshalled<string, int>("regexCallback", builder.ToString());
+        });
 
     [JSInvokable]
     [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
