d21dd6caedbce83a818611056e38da36b35f7bcb
Alberto Monteiro <alberto.monteiro@live.com>
2022-10-11 21:28:37 -0300

    Updated to dotnet 7.0.100-rc.2.22477.23

--- Regex101/Program.cs
+++ Regex101/Program.cs
@@ -1,1 +1,5 @@
 Regex101.Helpers.Init();
+_ = Regex101.Helpers.RegexMatches("", "", 0);
+_ = Regex101.Helpers.RegexMatch("", "", 0);
+_ = Regex101.Helpers.RegexReplace("", "", "", 0, true);
+_ = Regex101.Helpers.RegexListReplace("", "", "", 0, true);
---
+++ Regex101/blazorWebWorker.js
@@ -0,0 +1,47 @@
+import createDotnetRuntime from './dotnet.js'
+
+const { MONO, BINDING, Module, RuntimeBuildInfo } = await createDotnetRuntime();
+const appName = "Regex101";
+
+function bindStaticMethod(assembly, typeName, method) {
+    // Fully qualified name looks like this: "[debugger-test] Math:IntAdd"
+    const fqn = `[${assembly}] ${typeName}:${method}`;
+    return BINDING.bind_static_method(fqn);
+}
+
+self.addEventListener('message', (e) => {
+    const strJson = JSON.parse(e.data);
+    const flags = 0; //None
+    if (strJson.method === "globalMatches") {
+        const result = regexMatches(strJson.regex, strJson.textValue, flags);
+        self.postMessage(result);
+    }
+    else if (strJson.method === "oneMatch") {
+        const result = regexMatch(strJson.regex, strJson.textValue, flags);
+        self.postMessage(result);
+    }
+    else if (strJson.method === "substitution") {
+        const result = regexReplace(strJson.regex, strJson.textValue, strJson.substitution, flags, false);
+        self.postMessage(result);
+    }
+    else if (strJson.method === "listSubstitution") {
+        const result = regexListReplace(strJson.regex, strJson.textValue, strJson.substitution, flags, false);
+        self.postMessage(result);
+    }
+});
+
+
+self.engineInit = function engineInit() {
+    console.log("ready to run");
+    self.regexMatches = bindStaticMethod("browser", `${appName}.Helpers`, "RegexMatches");
+    self.regexMatch = bindStaticMethod("browser", `${appName}.Helpers`, "RegexMatch");
+    self.regexReplace = bindStaticMethod("browser", `${appName}.Helpers`, "RegexReplace");
+    self.regexListReplace = bindStaticMethod("browser", `${appName}.Helpers`, "RegexListReplace");
+}
+
+self.regexCallback = function regexCallback(value) {
+    const strJson = BINDING.conv_string(value);
+    self.postMessage(strJson);
+}
+
+self.engineInit();
